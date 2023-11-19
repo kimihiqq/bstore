e1e756a70fde06100de809639ba1421422b72fa4
kimihiqq <kimihiqq@gmail.com>
2023-11-20 02:29:55 +0900

    feat: Item 서비스 생성
    - 감소 로직 추가

---
+++ src/main/java/me/kimihiqq/bstore/domain/item/application/ItemService.java
@@ -0,0 +1,27 @@
+package me.kimihiqq.bstore.domain.item.application;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import me.kimihiqq.bstore.domain.item.domain.Item;
+import me.kimihiqq.bstore.domain.item.domain.ItemRepository;
+
+@Service
+public class ItemService {
+
+	private final ItemRepository itemRepository;
+
+	public ItemService(ItemRepository itemRepository) {
+		this.itemRepository = itemRepository;
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public void decrease(Long id, Long quantity) {
+		Item item = itemRepository.findById(id).orElseThrow();
+
+		item.removeStock(quantity);
+
+		itemRepository.saveAndFlush(item);
+	}
+}
