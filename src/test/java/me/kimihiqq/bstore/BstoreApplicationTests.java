d5ee04ef60f4a3af8d5a3b275f4ff88374587a4f
kimihiqq <kimihiqq@gmail.com>
2023-11-20 02:34:09 +0900

    test: Item 서비스 테스트 생성
    - concurrentOrderTest 추가

---
+++ src/test/java/me/kimihiqq/bstore/BstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package me.kimihiqq.bstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/me/kimihiqq/bstore/domain/item/application/ItemServiceTest.java
@@ -0,0 +1,63 @@
+package me.kimihiqq.bstore.domain.item.application;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import me.kimihiqq.bstore.domain.item.domain.Item;
+import me.kimihiqq.bstore.domain.item.domain.ItemRepository;
+
+@SpringBootTest
+class ItemServiceTest {
+
+	@Autowired
+	private ItemService itemService;
+
+	@Autowired
+	private ItemRepository itemRepository;
+
+	@BeforeEach
+	public void insert() {
+		Item item = new Item(1L, 100L);
+
+		itemRepository.saveAndFlush(item);
+	}
+
+	@AfterEach
+	public void delete() {
+		itemRepository.deleteAll();
+	}
+
+	@Test
+	@DisplayName("동시에 200명 주문")
+	public void concurrentOrderTest() throws InterruptedException {
+		int threadCount = 200;
+		ExecutorService executorService = Executors.newFixedThreadPool(32);
+		CountDownLatch latch = new CountDownLatch(threadCount);
+
+		for (int i = 0; i < threadCount; i++) {
+			executorService.submit(() -> {
+				try {
+					itemService.decrease(1L, 1L);
+				} finally {
+					latch.countDown();
+				}
+			});
+		}
+
+		latch.await();
+
+		Item item = itemRepository.findById(1L).orElseThrow();
+
+		assertEquals(0, item.getStockNumber());
+	}
+}
