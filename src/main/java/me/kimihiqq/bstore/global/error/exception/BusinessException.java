685a2655a23df39b7d8f162bddfd0e0ffda4f069
kimihiqq <kimihiqq@gmail.com>
2023-11-27 02:06:23 +0900

    feat: exception 패키지 생성 및 추가

---
+++ src/main/java/me/kimihiqq/bstore/global/error/exception/BusinessException.java
@@ -0,0 +1,12 @@
+package me.kimihiqq.bstore.global.error.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import me.kimihiqq.bstore.global.error.ErrorCode;
+
+@Getter
+@AllArgsConstructor
+public class BusinessException extends RuntimeException {
+
+	private final ErrorCode errorCode;
+}
