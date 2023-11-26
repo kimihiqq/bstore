eed0a062e13ccae2c2de2e016c64d3cf14cc9641
kimihiqq <kimihiqq@gmail.com>
2023-11-27 02:46:57 +0900

    feat: ErrorCode 추가

--- src/main/java/me/kimihiqq/bstore/global/error/ErrorCode.java
+++ src/main/java/me/kimihiqq/bstore/global/error/ErrorCode.java
@@ -10,6 +10,8 @@ public enum ErrorCode {
 	//서버
 	INTERNAL_SERVER_ERROR("S001", "예기치 못한 오류가 발생했습니다."),
 	UNABLE_TO_HANDLE_ERROR("S002", "처리할 수 없는 데이터입니다."),
+	THREAD_INTERRUPTED("S003", "스레드가 중단되었습니다."),
+
 
 	//공용
 	INVALID_INPUT_VALUE("C001", "잘못된 값을 입력하셨습니다."),
@@ -17,6 +19,7 @@ public enum ErrorCode {
 	DEFAULT_IMAGE_ALREADY_SET("C003", "기본 이미지로 설정된 이미지는 삭제할 수 없습니다."),
 	INVALID_LIST_SORT_TYPE("C004", "유효하지 않은 정렬 조건입니다."),
 	REQUIRE_QUERY_PARAM("C005", "URL에 추가적인 요청 조건이 필요합니다."),
+	LOCK_ACQUISITION_FAILED("L001", "락 획득에 실패했습니다."),
 
 	//상품
 	INVALID_ITEM_QUANTITY("I001", "상품 재고가 부족합니다"),
