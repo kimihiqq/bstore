4176ca082ea6912627692e8d50ef54e654fcd7df
kimihiqq <kimihiqq@gmail.com>
2023-11-27 02:08:47 +0900

    feat: Item 내 version 필드 추가

--- src/main/java/me/kimihiqq/bstore/domain/item/domain/Item.java
+++ src/main/java/me/kimihiqq/bstore/domain/item/domain/Item.java
@@ -1,6 +1,5 @@
 package me.kimihiqq.bstore.domain.item.domain;
 
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,13 +7,15 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
 import javax.persistence.Table;
+import javax.persistence.Version;
 
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import me.kimihiqq.bstore.global.BaseEntity;
-import me.kimihiqq.bstore.global.error.OutOfStockException;
+import me.kimihiqq.bstore.global.error.ErrorCode;
+import me.kimihiqq.bstore.global.error.exception.OutOfStockException;
 
 @Entity
 @Table(name="item")
@@ -34,6 +35,9 @@ public class Item extends BaseEntity {
 
 	private Long price;
 
+	@Version
+	private Long version;
+
 	@Column(nullable = false)
 	private Long stockNumber;
 
@@ -48,7 +52,7 @@ public class Item extends BaseEntity {
 	public void removeStock(Long stockNumber){
 		long restStock = this.stockNumber - stockNumber;
 		if(restStock<0){
-			throw new OutOfStockException("상품의 재고가 부족 합니다. (현재 재고 수량: " + this.stockNumber + ")");
+			throw new OutOfStockException(ErrorCode.INVALID_ITEM_QUANTITY);
 		}
 		this.stockNumber = restStock;
 	}
